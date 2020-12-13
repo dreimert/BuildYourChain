537ae38fe4d1005f72abba41dfb6f69e1b7f334f
Damien Reimert <damien.reimert@gmail.com>
2020-12-13 22:19:52 +0100

    Etape-1

--- cli.js
+++ cli.js
@@ -4,6 +4,8 @@ const argv = require('yargs') // Analyse des paramètres
   .command('get <key>', 'Récupère la valeur associé à la clé')
   .command('set <key> <value>', 'Place une association clé / valeur')
   .command('keys', 'Demande la liste des clés')
+  .command('addPeer <peerPort>', 'Ajoute un nouveau noeud voisin')
+  .command('peers', 'Demande la liste des pairs du noeud')
   .command('version', 'Demande la version du CLI')
   .option('url', {
     alias: 'u',
@@ -80,36 +82,78 @@ socket.on('connect', () => {
     console.info('Connection établie');
   }
 
-  switch (argv._[0]) {
-    case 'get':
-      if (!argv.bot) {
-        console.info(`Commande get ${argv.key} =>`);
-      }
-      socket.emit('get', argv.key, (value) => {
-        console.info(value);
+  let id;
+
+  // Returns a race between our timeout and the passed in promise
+  return Promise.race([
+    new Promise((resolve, reject) => {
+      id = setTimeout(() => {
+        clearTimeout(id);
         socket.close();
-      });
-      break;
-    case 'set':
-      if (!argv.bot) {
-        console.info(`set ${argv.key} =>`);
-      }
-      socket.emit('set', argv.key, argv.value, (ok) => {
-        console.info(ok);
+        reject('Le serveur ne répond pas');
+        if (!argv.bot) {
+          console.info(`Le serveur ne répond pas...`);
+        }
+      }, 5000)
+    }),
+  ,
+    new Promise((resolve, reject) => {
+      function end() {
         socket.close();
-      });
-      break;
-    case 'keys':
-      if (!argv.bot) {
-        console.info(`keys =>`);
+        resolve();
+        clearTimeout(id);
       }
-      socket.emit('keys', (keys) => {
-        console.info(keys.join(','));
-        socket.close();
-      });
-      break;
-    default:
-      console.error("Commande inconnue");
-      socket.close();
-  }
+
+      switch (argv._[0]) {
+        case 'get':
+          if (!argv.bot) {
+            console.info(`Commande get ${argv.key} =>`);
+          }
+          socket.emit('get', argv.key, (value) => {
+            console.info(value);
+            end();
+          });
+          break;
+        case 'set':
+          if (!argv.bot) {
+            console.info(`set ${argv.key} =>`);
+          }
+          socket.emit('set', argv.key, argv.value, (ok) => {
+            console.info(ok);
+            end();
+          });
+          break;
+        case 'keys':
+          if (!argv.bot) {
+            console.info(`keys =>`);
+          }
+          socket.emit('keys', (keys) => {
+            console.info(keys.join(','));
+            end();
+          });
+          break;
+        case 'addPeer':
+          if (!argv.bot) {
+            console.info(`addPeer ${argv.peerPort} =>`);
+          }
+          socket.emit('addPeer', argv.peerPort, (ok) => {
+            console.info(ok);
+            end();
+          });
+          break;
+        case 'peers':
+          if (!argv.bot) {
+            console.info(`peers =>`);
+          }
+          socket.emit('peers', (peers) => {
+            console.info(peers.join(','));
+            end();
+          });
+          break;
+        default:
+          console.error("Commande inconnue");
+          end();
+      }
+    }),
+  ])
 });
