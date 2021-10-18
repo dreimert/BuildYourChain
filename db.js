4152bb6417b82d44e11687bcc1afbffdb1c15efe
Damien Reimert <damien.reimert@gmail.com>
2021-10-18 10:42:21 +0200

    Etape-0

--- db.js
+++ db.js
@@ -11,51 +11,55 @@ const argv = require('yargs')
     description: 'Affiche la version de la db'
   })
   .help()
-  .argv;
+  .argv
 
 // Si l'utilisateur demande la verion
 if (argv.version) {
-  console.log("1.0.0");
-  process.exit(0); // met fin au programme
+  console.log('1.0.0')
+  process.exit(0) // met fin au programme
 }
 
 // Initialisation d'une socket
-function initSocket(socket) {
-  socket.on('get', function(field, callback){
-    console.info(`get ${field}: ${db[field]}`);
-    callback(db[field]); // lit et renvoie la valeur associée à la clef.
-  });
+function initSocket (socket) {
+  socket.on('get', function (field, callback) {
+    console.info(`get ${field}: ${db[field]}`)
+    callback(db[field]) // lit et renvoie la valeur associée à la clef.
+  })
 
-  socket.on('set', function(field, value, callback){
+  socket.on('set', function (field, value, callback) {
     if (field in db) { // Si la clef est dans la base de donnée
-      console.info(`set error : Field ${field} exists.`);
-      callback(true);
+      if (db[field] === value) {
+        callback(true)
+      } else {
+        console.info(`set error : Field ${field} exists.`)
+        callback(false)
+      }
     } else {
-      console.info(`set ${field} : ${value}`);
-      db[field] = value;
-      callback(true);
+      console.info(`set ${field} : ${value}`)
+      db[field] = value
+      callback(true)
     }
-  });
+  })
 
-  socket.on('keys', function(callback){
-    console.info(`keys`);
-    callback(Object.keys(db)); // Object.keys() extrait la liste des clefs d'un object et les renvoie sous forme d'un tableau.
-  });
+  socket.on('keys', function (callback) {
+    console.info(`keys`)
+    callback(Object.keys(db)) // Object.keys() extrait la liste des clefs d'un object et les renvoie sous forme d'un tableau.
+  })
 }
 
 // Création du serveur
 const io = require('socket.io')(argv.port, {
   path: '/byc',
-  serveClient: false,
-});
+  serveClient: false
+})
 
-console.info(`Serveur lancé sur le port ${argv.port}.`);
+console.info(`Serveur lancé sur le port ${argv.port}.`)
 
 // Création de la DB
-const db = Object.create(null);
+const db = Object.create(null)
 
 // À chaque nouvelle connexion
 io.on('connect', (socket) => {
-  console.info('Nouvelle connexion');
-  initSocket(socket);
-});
+  console.info('Nouvelle connexion')
+  initSocket(socket)
+})
